22b314becb51dbf58552f7f7c12941bcf813a93e
Brendan Hanna <gepetto503@gmail.com>
2017-04-28 11:07:04 -0700

    add hilarious spicoli picture and warning message to pop up if none of the if statements are true

--- js/scripts.js
+++ js/scripts.js
@@ -4,21 +4,21 @@ $(document).ready(function() {
 
     event.preventDefault();
 
-    var frontBack = $("input:radio[name=front-back]:checked").val();
+    var lookWork = $("input:radio[name=front-back]:checked").val();
 
     var coSize = $("input:radio[name=co-size]:checked").val();
 
-    var likeApps = $("input:radio[name=app]:checked").val();
+    var appSite = $("input:radio[name=app-site]:checked").val();
 
-    if (frontBack === "look") {
+    if (lookWork === "look") {
       $(".results").hide();
       $("#css").show();
-    } else if (likeApps === "yes") {
+    } else if (appSite === "app") {
       $(".results").hide();
       $("#ruby").show();
     } else {
       $(".results").hide();
-      $("#c-sharp").show();
+      $("#uhoh").show();
     };
 
 
