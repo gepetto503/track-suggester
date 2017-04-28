a89d5ab264e381a9ea8019a28b0189f0de6f7eeb
Brendan Hanna <gepetto503@gmail.com>
2017-04-28 13:29:42 -0700

    cause spicoli warning message to pop up if user doesn't fill any fields in

--- js/scripts.js
+++ js/scripts.js
@@ -5,13 +5,9 @@ $(document).ready(function() {
     event.preventDefault();
 
     var lookWork = $("input:radio[name=front-back]:checked").val();
-
     var coSize = $("input:radio[name=cosize]:checked").val();
-
     var appSite = $("input:radio[name=app-site]:checked").val();
-
     var androidBase = $("input:radio[name=android-base]:checked").val();
-
     var likeComps = $("input:radio[name=likecomps]:checked").val();
 
     if (likeComps === "no") {
@@ -29,9 +25,12 @@ $(document).ready(function() {
     }else if (coSize === "big" && androidBase === "android") {
       $(".results").hide();
       $("#java").show();
-    } else {
+    } else if (coSize === "small" && appSite === "site") {
       $(".results").hide();
       $("#php").show();
+    } else {
+      $(".results").hide();
+      $("#uhoh").show();
     };
 
 
