0b913297bd650bcce623765f3a03492257f42485
Brendan Hanna <gepetto503@gmail.com>
2017-04-28 10:29:46 -0700

    add third question in form with two options.  add if else logic such that if user picks how things look from question one or nah, i'm good from question 3, they get shown a div with the result css or ruby, respectively.  If user avoids those two options, they will be shown a div with the result c#.

--- js/scripts.js
+++ js/scripts.js
@@ -8,9 +8,14 @@ $(document).ready(function() {
 
     var coSize = $("input:radio[name=co-size]:checked").val();
 
+    var likeApps = $("input:radio[name=app]:checked").val();
+
     if (frontBack === "look") {
       $(".results").hide();
       $("#css").show();
+    } else if (likeApps === "yes") {
+      $(".results").hide();
+      $("#ruby").show();
     } else {
       $(".results").hide();
       $("#c-sharp").show();
