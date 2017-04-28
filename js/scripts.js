da70575176a1ffc5925f501c1d9d2552ff738934
Brendan Hanna <gepetto503@gmail.com>
2017-04-28 11:24:47 -0700

    add a 4th question and corresponding js logic

--- js/scripts.js
+++ js/scripts.js
@@ -6,9 +6,11 @@ $(document).ready(function() {
 
     var lookWork = $("input:radio[name=front-back]:checked").val();
 
-    var coSize = $("input:radio[name=co-size]:checked").val();
+    var coSize = $("input:radio[name=cosize]:checked").val();
 
     var appSite = $("input:radio[name=app-site]:checked").val();
+
+    var androidBase = $("input:radio[name=android-base]:checked").val();
 
     if (lookWork === "look") {
       $(".results").hide();
@@ -16,6 +18,15 @@ $(document).ready(function() {
     } else if (appSite === "app") {
       $(".results").hide();
       $("#ruby").show();
+    } else if (coSize === "big" && androidBase === "base") {
+      $(".results").hide();
+      $("#c-sharp").show();
+    }else if (coSize === "big" && androidBase === "android") {
+      $(".results").hide();
+      $("#java").show();
+    } else if (true) {
+      $(".results").hide();
+      $("#php").show();
     } else {
       $(".results").hide();
       $("#uhoh").show();
