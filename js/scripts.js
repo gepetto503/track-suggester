f90e2c2090b9f0a31d8fd1d401ab49788a487f76
Brendan Hanna <gepetto503@gmail.com>
2017-04-28 10:04:02 -0700

    create a form with 2 questions, each with 2 radio inputs.  in scripts.js, uses a function on the .submit event handler targetting the form which assigns variables to the values of each radio.  function then has if-else statement that will show one previously hidden div if radio with value look is checked, and show a different hidden div if it's not.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,22 @@
 $(document).ready(function() {
 
+  $("form#userForm").submit(function(event) {
+
+    event.preventDefault();
+
+    var frontBack = $("input:radio[name=front-back]:checked").val();
+
+    var coSize = $("input:radio[name=co-size]:checked").val();
+
+    if (frontBack === "look") {
+      $(".results").hide();
+      $("#css").show();
+    } else {
+      $(".results").hide();
+      $("#c-sharp").show();
+    };
+
+
+  });
+
 });
